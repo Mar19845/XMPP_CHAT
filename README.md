12110136fa6ae422a1f2a26c9cf0dff5b15d17ac
Mar19845 <mar19845@uvg.edu.gt>
2022-08-16 18:50:21 -0600

    finish lab

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
+
 # XMPP_CHAT
+
+Simple xmpp chat develop in python and slixmpp
+
+## Install packages
+
+pip install -r requirements.txt
+## Description
+
+The project has the following funcions:
+
+1. send msg to 1 user
+2. show status of users
+3. add new user
+4. show status of 1 user
+5. groupchat (almost working)
+6. define presence and status
+7. delete account
+8. loggout
+9. create new account
+
+## Run app
+
+ python .\main.py  
--- client.py
+++ client.py
@@ -22,94 +22,172 @@ class Client(slixmpp.ClientXMPP):
         self.user = jid
         self.presences = threading.Event()
         
-        #add evetns handlers
         self.add_event_handler("session_start", self.start)
-        self.add_event_handler("register", self.register)
-        self.add_event_handler("delete", self.delete)
-        self.add_event_handler("message", self.message)
-        self.add_event_handler("add_contact", self.add_contact)
-        self.add_event_handler("groupchat_message", self.groupchat)
-        self.add_event_handler("change_status", self.change_status)
         
-    #loggin   
-    def start(self, event):
+    async def start(self, event):
+        
         self.send_presence()
-        self.get_roster()
-        self.disconnect()
-    #register user
-    def register(self, iq):
-        self.send_presence()
-        self.get_roster()
-        iq = self.Iq()
-        iq['type'] = 'set'
-        iq['register']['username'] = self.boundjid.user
-        iq['register']['password'] = self.password
-
-        try:
-            iq.send()
-            print("Nueva Cuenta Creada", self.boundjid,"\n")
-        except IqError as e:
-            print("Error en Registro ", e,"\n")
-            self.disconnect()
-        except IqTimeout:
-            print("Timeout en el servidor")
-            self.disconnect()
-        except Exception as e:
-            print(e)
-            self.disconnect()
+        #send gruop msg
+        def send_groupchat_msg():
+            self.register_plugin('xep_0030')
+            self.register_plugin('xep_0045')
+            self.register_plugin('xep_0199')
+            room = input('room -> ')
+            nickname = input('nickname -> ')
+            self.plugin['xep_0045'].join_muc(room, nickname)
+            cond = True
+            while cond:
+                try:
+                    msg = input('msg -> ')
+                    self.send_message(mto=self.room, mbody=msg, mtype='groupchat')
+                    self.send_presence()
+                except KeyboardInterrupt as e:
+                    print('Conversacion finalizada')
+                    cond = False
+        #send private msg to a user
+        def send_private_msg():
             self.send_presence()
-        self.get_roster()
-    #delete user from server 
-    def delete(self, event):
-  
-        self.send_presence()
-        self.get_roster()
+            user = input('To -> ')
+            msg = input('msg -> ')
+            self.send_message(mto= user, mbody= msg, mtype='chat')
+            print('msg send \n')
+            self.send_presence()
+        #logot out func  
+        def logout():
+            self.disconnect()
+            print("session ended \n")
+            
+        #add new user func
+        def add_user():
+            user = input('user -> ')
+            self.send_presence_subscription(pto=user)
+            
+        #delete account from server func
+        def delete_account():
+            self.register_plugin('xep_0030') 
+            self.register_plugin('xep_0004')
+            self.register_plugin('xep_0077')
+            self.register_plugin('xep_0199')
+            self.register_plugin('xep_0066')
 
-        delete = self.Iq()
-        delete['type'] = 'set'
-        delete['from'] = self.user
-        fragment = ET.fromstring("<query xmlns='jabber:iq:register'><remove/></query>")
-        delete.append(fragment)
-
-        try:
+            # delete logged account
+            delete = self.Iq()
+            delete['type'] = 'set'
+            delete['from'] = self.boundjid.user
+            delete['register']['remove'] = True
             delete.send()
-            print("Cuenta Borrada")
-        except IqError as e:
-           
-            print("Error", e)
-        except IqTimeout:
-
-            print("timeout del server")
-        except Exception as e:
-     
-            print(e)  
-
-        self.disconnect()
-    #send msg to a person in server
-    def message(self, to,msg):
-        print(msg)
-        self.send_presence()
+            
+            self.disconnect()
+            print('Account successfully deleted\n')
         
-        self.get_roster()
-        self.send_message(mto=to,mbody=msg,mtype='chat')
-    #add new contact
-    def add_contact(self,user):
-        self.send_presence()
-        self.get_roster()
-        try:
-           self.send_presence_subscription(pto=user) 
-        except IqTimeout:
-            print("Timeout del server")
-        self.send_presence()
-        self.get_roster()
-    async def groupchat(self,room,message):
-        self.send_presence()
-        self.get_roster()
-        self.send_message(mto=room, mbody=message, mtype='groupchat')
+        #show friends info func
+        def show_users():
+            self.send_presence()
+            self.get_roster()
+            print('================================================\n')
+            contacts = self.client_roster.groups()
+            for contact in contacts:
+                for jid in contacts[contact]:
+                    user = self.client_roster[jid]['name']
+                    
+                    print('-----------------------------')
+
+                    user_connected = self.client_roster.presence(jid)
+                    for res, pres in user_connected.items():
+                        print('Usuario: ', jid)
+                        show = 'conectado'
+                        if pres['show']:
+                            show = pres['show']
+                        print(show)
+                        if pres['status']:
+                            print(' Status -> ', pres['status'])
+                        print('-----------------------------')
+
+        #change status on server            
+        def change_status():
+            self.send_presence()
+            self.get_roster()
+            status = input("status -> ")
+            info = input("your info -> ")
+            self.send_presence(pshow=info, pstatus=status)
+            print('Status changed successfully\n')
         
-    def change_status(self,msg):
-        self.send_presence()
-        self.get_roster()
-        self.notification(self.message, 'active')
+        def user_detail():
+            self.send_presence()
+            self.get_roster()
+            user = input("user -> ")
+            contacts = self.client_roster.presence(user)
+            for res, pres in contacts.items():
+                show = 'chat'
+                if pres['show']:
+                    show = pres['show']
+                print(" INFO:")
+                print(show)
+                print('Status -> ', pres['status'])
+            
+        
+        cond = True
+        while cond:
+            print("1. Send a msg to a user") # done
+            print("2. show status and details of all friends") # done
+            print("3. add new user") # done
+            print("4. show detail and status of a user") # done
+            print("5. groupchat")
+            print("6. define status") # done
+            print("7. delete account") # done
+            print("8. loggout") # done
+            opc = input('-> ')
+            
+            if opc == '8':
+                logout()
+                cond = False
+            elif opc == '1':
+                send_private_msg()
+            elif opc == '2':
+                show_users()
+            elif opc == '3':
+                add_user()
+            elif opc == '4':
+                user_detail()
+            elif opc == '5':
+                send_groupchat_msg()
+            elif opc == '6':
+                change_status()
+            elif opc == '7':
+                delete_account()
+                cond = False
+            self.send_presence()
+            await self.get_roster()
+                
     
-    
+    def loggin():
+        
+
+        user = input("User: ")
+        #psd = getpass("Password : ")
+        psd = input("Password : ")
+
+        client = Client(user, psd)
+        client.register_plugin('xep_0030') 
+        client.register_plugin('xep_0199') 
+
+        # Connect to the XMPP server and start processing XMPP stanzas.
+        client.connect()
+        client.process(forever=False)
+        
+    def signin():
+        user = input("User: ")
+        #psd = getpass("Password : ")
+        psd = input("Password : ")
+
+        client = Client(user, psd)
+        client.register_plugin('xep_0030') 
+        client.register_plugin('xep_0004')
+        client.register_plugin('xep_0077')
+        client.register_plugin('xep_0199')
+        client.register_plugin('xep_0066')
+        client["xep_0077"].force_registration = True
+
+        # Connect to the XMPP server and start processing XMPP stanzas.
+        client.connect()
+        client.process(forever=False)
